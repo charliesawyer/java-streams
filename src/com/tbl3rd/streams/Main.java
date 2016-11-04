6b26ce1f2c36ce6a9946f8aeb3d0449eae6c8a82
Tom Lyons <lyonst@vmware.com>
2016-11-04 17:09:32 -0400

    Factor out collate.

--- src/com/tbl3rd/streams/Main.java
+++ src/com/tbl3rd/streams/Main.java
@@ -3,14 +3,14 @@ package com.tbl3rd.streams;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.MessageFormat;
 import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static java.text.MessageFormat.*;
+import static java.text.MessageFormat.format;
 
 public class Main {
 
@@ -42,23 +42,27 @@ public class Main {
             indexes.entrySet().stream().collect(
                 Collectors.groupingBy(
                     Map.Entry::getKey,
-                    Collectors.toMap(
-                        v -> fileName,
-                        Map.Entry::getValue)));
+                    Collectors.toMap(v -> fileName,
+                                     Map.Entry::getValue)));
         result.forEach((name, index) -> System.out.println(
                            format("{0}: {1}", name, index)));
         return result;
     }
 
-    public static void main(String[] argv) {
-        Map<String, List<Map<String, List<Integer>>>> index =
-            Arrays.stream(argv)
-            .map(Main::indexWordsInFile)
+    private static Map<String, List<Map<String, List<Integer>>>> collate(
+        Function<String, Map<String, Map<String, List<Integer>>>> mapWords,
+        String[] fileNames) {
+        return Arrays.stream(fileNames).map(mapWords)
             .map(Map::entrySet).flatMap(Collection::stream)
             .collect(Collectors.groupingBy(
                          Map.Entry::getKey,
                          Collectors.mapping(Map.Entry::getValue,
                                             Collectors.toList())));
+    }
+
+    public static void main(String[] argv) {
+        final Map<String, List<Map<String, List<Integer>>>> index =
+            collate(Main::indexWordsInFile, argv);
         index.entrySet().stream()
             .sorted(Comparator.comparing(Map.Entry::getKey))
             .forEach(e -> System.out.println(
